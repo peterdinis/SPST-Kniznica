3a0e424d288367f83d671f45ff7ef1709b475d0a
Peterdinis <peterdinis0@pobox.sk>
2023-02-01 18:57:51 +0100

    feat: Adding react-query wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import Layout from "@/components/shared/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Suspense fallback={<FallbackLoader />}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </Suspense>
   );
 }

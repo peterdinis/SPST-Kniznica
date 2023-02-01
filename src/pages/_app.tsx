945ea0f835c262862299d5120f9bd06e8d34e1d9
Peterdinis <peterdinis0@pobox.sk>
2023-02-01 13:41:38 +0100

    FOO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import Layout from '@/components/shared/Layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import FallbackLoader from "@/components/shared/FallbackLoader";
+import Layout from "@/components/shared/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Suspense } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <Suspense fallback={<FallbackLoader />}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Suspense>
+  );
 }

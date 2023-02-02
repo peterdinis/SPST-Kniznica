394f7c627f1b8638253659fa8bdcb02ed51cc9af
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 13:35:24 +0100

    FOO

--- src/components/books/BookInfo.tsx
+++ src/components/books/BookInfo.tsx
@@ -1,7 +1,9 @@
+import Header from "../shared/Header";
+
 const BookInfo: React.FC = () => {
     return (
         <>
-        
+          <Header name="Detail Knihy" />
         </>
     )
 }
--- src/components/category/CategoryInfo.tsx
+++ src/components/category/CategoryInfo.tsx
@@ -1,7 +1,28 @@
+import Header from "../shared/Header";
+import * as api from "../../api/queries/categoryQueries"
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+
 const CategoryInfo: React.FC = () => {
+    const router = useRouter();
+
+    const {id}= router.query;
+
+    console.log(Number(id));
+
+    const { data, isError } = useQuery(
+        ['categoryDetail', Number(id)],
+        () => api.getOneCategory(Number(id) as any));
+
+
+    console.log(data);
+
+    const navigateToCategories = () => {
+        router.push("/category");
+    }
     return (
         <>
-        eeee
+         <Header name="Detail Kategórie" />
         </>
     )
 }

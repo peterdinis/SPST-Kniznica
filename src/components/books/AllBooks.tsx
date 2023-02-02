99d410a8d3ab54cea5e2c48cd72ba9605bcb0abc
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 10:57:32 +0100

    feat: Updating books and categories

--- src/components/books/AllBooks.tsx
+++ src/components/books/AllBooks.tsx
@@ -4,6 +4,8 @@ import * as api from "../../api/queries/bookQueries";
 import FallbackLoader from "../shared/FallbackLoader";
 import FallbackRender from "../shared/FallbackRender";
 import Link from "next/link";
+import ScrollToTop from "@/hooks/useScroll";
+import { IBook } from "@/api/interfaces";
 
 const AllBooks: React.FC = () => {
   const { data, isError, isLoading } = useQuery(["allBooks"], api.getBooks);
@@ -15,42 +17,44 @@ const AllBooks: React.FC = () => {
     return <FallbackRender error="Nastala chyba" />;
   }
 
-  console.log(data);
-
   return (
     <>
       <Header name="Všetky knihy" />
-     <div className="grid gap-8 space-x-1 lg:grid-cols-6">
-     {data &&
-        data.map((item: any) => {
-          return (
-            <>
-              <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
-                <div className="mb-8">
-                  <img
-                    alt="Placeholder"
-                    className="h-auto w-full"
-                    src={item.image}
-                  />
-                </div>
-                <div className="text-center">
-                  <h3 className="text-2xl text-gray-800">
-                    {item.name} - {item.author}
-                  </h3>
-                  <div className="text-center mt-4">
-                    <Link
-                      className="link mt-10 bg-blue-200 p-2 rounded"
-                      href={`/book/${item.id}`}
-                    >
-                      Detail
-                    </Link>
+      <div className="grid gap-8 space-x-1 lg:grid-cols-6">
+        {data &&
+          data.map((item: IBook, i: IBook) => {
+            return (
+              <>
+                <div
+                  key={i.id}
+                  className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center"
+                >
+                  <div className="mb-8">
+                    <img
+                      alt="Placeholder"
+                      className="h-auto w-full"
+                      src={item.image}
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h3 className="text-2xl text-gray-800">
+                      {item.name} - {item.author}
+                    </h3>
+                    <div className="text-center mt-4">
+                      <Link
+                        className="link mt-10 bg-blue-200 p-2 rounded"
+                        href={`/book/${item.id}`}
+                      >
+                        Detail
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
-     </div>
+              </>
+            );
+          })}
+        <ScrollToTop />
+      </div>
     </>
   );
 };
--- src/components/category/AllCategory.tsx
+++ src/components/category/AllCategory.tsx
@@ -1,12 +1,50 @@
 import Header from "../shared/Header";
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import * as api from "../../api/queries/categoryQueries";
+import { ICategory } from "@/api/interfaces";
+import FallbackLoader from "../shared/FallbackLoader";
+import FallbackRender from "../shared/FallbackRender";
 
 const AllCategory: React.FC = () => {
-    return (
-        <>
-         <Header name="Všetky kategórie" />
-        </>
-    )
-}
+  const { data, isLoading, isError } = useQuery(
+    ["categories"],
+    api.getCategories
+  );
 
+  if (isLoading) {
+    return <FallbackLoader />;
+  }
+  if (isError) {
+    return <FallbackRender error="Nastala chyba" />;
+  }
+
+  return (
+    <>
+      <Header name="Všetky kategórie" />
+      <div className="w-full mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        {data &&
+          data.map((item: ICategory) => {
+            return (
+              <>
+                <div className="bg-whiterounded py-5 pl-6 flex items-start shadow">
+                  <div className="pl-3 pr-10 mt-1">
+                    <h3 className="font-normal leading-4 text-red-800 text-2xl">
+                      <Link href={`/category/${item.id}`}>{item.name}</Link>
+                    </h3>
+                    <h3 className="mt-4 leading-4 text-gray-800 text-lg">
+                      <Link href={`/category/${item.id}`}>
+                        {item.description}
+                      </Link>
+                    </h3>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </>
+  );
+};
 
 export default AllCategory;

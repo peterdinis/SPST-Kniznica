24409ed4b9866ecdec79941e701bd547fde146e0
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 13:45:09 +0100

    feat: Create component for book detail

--- src/components/books/BookInfo.tsx
+++ src/components/books/BookInfo.tsx
@@ -21,6 +21,45 @@ const BookInfo: React.FC = () => {
   return (
     <>
       <Header name="Detail Knihy" />
+      <section className="mt-2 text-gray-700 body-font overflow-hidden bg-white">
+        <div className="container px-5 py-12 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            {data.image === null ||
+            data.image === undefined ||
+            !data.image ||
+            data.image === "string" ? (
+              <img
+                alt="No Image"
+                className="lg:w-1/2 w-full object-cover object-center rounded-lg border drop-shadow-md"
+                src="https://bitsofco.de/content/images/2018/12/broken-1.png"
+              />
+            ) : (
+              <img
+                alt="ecommerce"
+                className="lg:w-1/2 w-full object-cover object-center rounded-lg border drop-shadow-md"
+                src={data.image}
+              />
+            )}
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <div>
+                <h1 className="text-gray-900 text-4xl title-font font-medium mb-1">
+                  <span className="font-bold">Názov</span>: {data.name}
+                </h1>
+              </div>
+              <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                <span className="font-bold">Krátke info</span>:{" "}
+                {data.description}
+              </p>
+              <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                <span className="font-bold">Author</span>: {data.author}
+              </p>
+              <p className="text-2xl mt-3 font-light leading-relaxed  mb-4 text-gray-800">
+                <span className="font-bold"> Rok</span>: {data.year}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };

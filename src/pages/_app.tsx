64468bd3852718939a035c4bbdd03b6edae218df
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 11:04:03 +0100

    FOO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,17 @@
-import FallbackLoader from "@/components/shared/FallbackLoader";
 import Layout from "@/components/shared/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
-import {  QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/api/queryClient";
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Suspense fallback={<FallbackLoader />}>
-      <QueryClientProvider client={queryClient}>
-        <Layout>
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
-        </Layout>
-      </QueryClientProvider>
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </Layout>
+    </QueryClientProvider>
   );
 }

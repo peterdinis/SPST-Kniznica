bc0ad2b826c5d8de30306a37d42f08b8f7eb9799
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 14:33:55 +0100

    feat: Clerk helper package wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,17 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/api/queryClient";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </Layout>
+      <ClerkProvider {...pageProps}>
+        <Layout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </Layout>
+      </ClerkProvider>
     </QueryClientProvider>
   );
 }

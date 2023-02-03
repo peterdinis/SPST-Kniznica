6e8127cf0d5eae09b54d1f7dc5df25a541046e50
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 11:09:55 +0100

    fix: Fixing images wip

--- src/components/category/CategoryInfo.tsx
+++ src/components/category/CategoryInfo.tsx
@@ -19,7 +19,7 @@ const CategoryInfo: React.FC = () => {
   );
 
   if(isError) {
-    return <FallbackRender error="Something went wrong" />
+    return <FallbackRender error="Nastala chyba" />
   }
 
   if(isLoading) {
@@ -29,9 +29,32 @@ const CategoryInfo: React.FC = () => {
   const navigateToCategories = () => {
     router.push("/category");
   };
+
+  console.log(data);
+
   return (
     <>
       <Header name="Detail Kategórie" />
+      <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Meno kategórie</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {data.name}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Popis kategórie
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {data.description}
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
     </>
   );
 };

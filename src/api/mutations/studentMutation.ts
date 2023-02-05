0bfeb19548f8c6dbc10637830a3b3c8b1f339e8c
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 11:12:19 +0100

    FOO

---
+++ src/api/mutations/studentMutation.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
+});
+
+export const registerStudent = () => {
+  return;
+};
+
+export const loginStudent = () => {
+  return;
+};
---
+++ src/store/authStore.ts
@@ -0,0 +1,5 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+/* TODO: Update this later  */
+export const useAuthStore = create();

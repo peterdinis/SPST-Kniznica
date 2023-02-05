69765f06677eed75b6fdc4fda3f67929b6a8ed28
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 13:08:51 +0100

    feat: NEw files

--- src/api/interfaces/IBooking.ts
+++
@@ -1,7 +0,0 @@
-export interface IBooking {
-    from: Date | String;
-    to: Date | String;
-    bookId: number;
-    userId: number;
-    extended?: boolean;
-}
--- src/api/interfaces/index.ts
+++
@@ -1,7 +0,0 @@
-import { IBook } from "./IBook";
-import {ICategory} from "./ICategory";
-
-export {
-    IBook,
-    ICategory,
-}

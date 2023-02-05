9636f4454ec9c34d39325109d322fd4cd3cc1d88
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 09:51:43 +0100

    fix: Fixing small problem in categoryInfo

--- src/components/category/CategoryInfo.tsx
+++ src/components/category/CategoryInfo.tsx
@@ -13,49 +13,63 @@ const CategoryInfo: React.FC = () => {
 
   const { data, isError, isLoading } = useQuery(
     ["categoryDetail", Number(id)],
-    () => api.getOneCategory(Number(id) as any), {
-        retry: 2,
-        placeholderData: placeholderCategory
+    () => api.getOneCategory(Number(id) as any),
+    {
+      retry: 2,
+      placeholderData: placeholderCategory,
     }
   );
 
-  if(isError) {
-    return <FallbackRender error="Nastala chyba" />
+  if (isError) {
+    return <FallbackRender error="Nastala chyba" />;
   }
 
-  if(isLoading) {
-    return <FallbackLoader />
+  if (isLoading) {
+    return <FallbackLoader />;
   }
 
   const navigateToCategories = () => {
     router.push("/category");
   };
-
-  console.log(data);
-
   return (
     <>
       <Header name="Detail Kategórie" />
       <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-          <div className="border-t border-gray-200">
-            <dl>
-              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Meno kategórie</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                  {data.name}
-                </dd>
-              </div>
-              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">
-                  Popis kategórie
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                  {data.description}
-                </dd>
-              </div>
-            </dl>
-          </div>
+        <div className="border-t mt-4 border-gray-200">
+          <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Meno kategórie
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data[0].name}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Popis kategórie
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data[0].description}
+              </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Počet kníh ktoré majú túto kategóriu
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                EEEEEEEEEEEEEEEe
+              </dd>
+            </div>
+          </dl>
+          <button
+            onClick={navigateToCategories}
+            className="mt-6 bg-blue-200 rounded-lg p-2 font-extrabold"
+          >
+            Späť na kategórie
+          </button>
         </div>
+      </div>
     </>
   );
 };

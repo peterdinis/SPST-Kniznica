c871d97b9c3c5ca2d8b65eed385e9f1fea420831
Peterdinis <peterdinis0@pobox.sk>
2023-02-06 19:31:39 +0100

    feat: Adding next-auth package wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,20 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/api/queryClient";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "@next/font/google";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ClerkProvider {...pageProps}>
+      <SessionProvider session={session}>
         <div className={inter.className}>
           <Layout>
             <Component {...pageProps} />
@@ -22,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <ReactQueryDevtools />
           </Layout>
         </div>
-      </ClerkProvider>
+      </SessionProvider>
     </QueryClientProvider>
   );
 }

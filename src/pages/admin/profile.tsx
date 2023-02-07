4704221006bc5231713e0c5b004f718e72eaebf6
Peterdinis <peterdinis0@pobox.sk>
2023-02-07 15:22:22 +0100

    feat: Adding new pages wip

---
+++ src/pages/admin/profile.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const AdminProfilePage: NextPage = () => {
+    return (
+        <>
+        rrrr
+        </>
+    )
+}
+
+export default AdminProfilePage
---
+++ src/pages/admin/register.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const AdminRegisterPage: NextPage = () => {
+    return (
+        <>
+        rrrr
+        </>
+    )
+}
+
+export default AdminRegisterPage

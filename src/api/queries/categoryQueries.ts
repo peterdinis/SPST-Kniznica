737a78b431cea41efdebd9bb36c6b3c5c9b1055e
Peterdinis <peterdinis0@pobox.sk>
2023-02-07 15:25:49 +0100

    FOO

--- src/api/queries/categoryQueries.ts
+++ src/api/queries/categoryQueries.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
 });
 
-export const getCategories = () => api.get("category").then((res) => res.data);
+export const getCategories = () => api.get("categories").then((res) => res.data);
 
 export const getOneCategory = (id: string) => {
   if (!id) {

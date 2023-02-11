06ae91a24b6e650c3f018248c83fe49cb886142e
Peterdinis <peterdinis0@pobox.sk>
2023-02-11 15:06:05 +0100

    feat: Start breaking books logic wip

---
+++ src/pages/books/all.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const AllBooksPage: NextPage = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+export default AllBooksPage;
---
+++ src/pages/books/search.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const BooksSearchPage: NextPage = () => {
+    return (
+        <>
+        rrrrrrrrrrr
+        </>
+    )
+}
+
+export default BooksSearchPage;

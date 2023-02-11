32e8514802ca5ca3dfd23c9d6c5f6cf823c9cefc
Peterdinis <peterdinis0@pobox.sk>
2023-02-11 09:48:14 +0100

    FOO

--- src/api/queries/exampleQuery.ts
+++ src/api/queries/exampleQuery.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string
 });
 
 export const getExampleData = () => api.get("example").then((res) => res.data);

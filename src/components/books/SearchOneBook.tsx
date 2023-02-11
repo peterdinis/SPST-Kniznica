0d2c48f7052cea46f4e5122433b6b7f644640e8a
Peterdinis <peterdinis0@pobox.sk>
2023-02-11 19:10:00 +0100

    feat: Fixing bug in book search bar

--- src/components/books/SearchOneBook.tsx
+++ src/components/books/SearchOneBook.tsx
@@ -56,43 +56,46 @@ const SearchOneBook: React.FC = () => {
       </div>
 
       <AnimatePresence>
-        {results.data === undefined && (
+        {results.data === undefined ? (
           <div className="text-center mt-4 font-bold text-xl">
             Hľadám
-            <SearchIcon />
+            <SearchIcon />...
           </div>
-        )}
-        <div className="grid gap-8 space-x-1 lg:grid-cols-6">
-          {/*  {results.data.map &&
-            results.data.map((item: IBook) => {
-              return (
-                <>
-                  <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
-                    <div className="mb-8">
-                      <img
-                        alt="Placeholder"
-                        className="h-auto w-full"
-                        src={item.image}
-                      />
-                    </div>
-                    <div className="text-center">
-                      <h3 className="text-2xl text-gray-800">
-                        {item.name} - {item.author}
-                      </h3>
-                      <div className="text-center mt-4">
-                        <Link
-                          className="link mt-10 bg-blue-200 p-2 rounded"
-                          href={`/books/${item.id}`}
-                        >
-                          Detail
-                        </Link>
+        ) : (
+          <>
+            <div className="grid gap-8 space-x-1 lg:grid-cols-6">
+              {results.data.map &&
+                results.data.map((item: IBook) => {
+                  return (
+                    <>
+                      <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                        <div className="mb-8">
+                          <img
+                            alt="Placeholder"
+                            className="h-auto w-full"
+                            src={item.image}
+                          />
+                        </div>
+                        <div className="text-center">
+                          <h3 className="text-2xl text-gray-800">
+                            {item.name} - {item.author}
+                          </h3>
+                          <div className="text-center mt-4">
+                            <Link
+                              className="link mt-10 bg-blue-200 p-2 rounded"
+                              href={`/books/${item.id}`}
+                            >
+                              Detail
+                            </Link>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </>
-              );
-            })} */}
-        </div>
+                    </>
+                  );
+                })}
+            </div>
+          </>
+        )}
       </AnimatePresence>
     </>
   );

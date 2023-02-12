4c23b4dfb24f157d50ae452b83420215a5f73a9a
Peterdinis <peterdinis0@pobox.sk>
2023-02-12 13:38:32 +0100

    FOO

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -1,7 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 import { queryCache } from "./cache/queryCache";
 import { mutationCache } from "./cache/mutationCache";
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 
@@ -14,25 +13,10 @@ export const queryClient = new QueryClient({
       refetchOnReconnect: false,
       retry: false,
       staleTime: twentyFourHoursInMs,
-
     },
     mutations: {
       networkMode: "offlineFirst",
       
     },
-  },
-  logger: {
-    log: (...args) => {
-      console.log(...args);
-      console.log("Log info");
-    },
-    warn: (...args) => {
-      console.log(...args);
-      console.log("Warn info");
-    },
-    error: (...args) => {
-      console.log(...args);
-      console.log("Error info");
-    },
-  },
+  }
 });

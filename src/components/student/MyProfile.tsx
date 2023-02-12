1e10f6ad6cac0e6e941b34aca3dce0e5e589013d
Peterdinis <peterdinis0@pobox.sk>
2023-02-12 19:35:08 +0100

    feat: Updating profile component

--- src/components/student/MyProfile.tsx
+++ src/components/student/MyProfile.tsx
@@ -2,17 +2,23 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import FallbackLoader from "../shared/FallbackLoader";
 import FallbackRender from "../shared/FallbackRender";
+import { useMutation } from "@tanstack/react-query";
+import * as api from "../../api/mutations/studentMutation";
+import {toast} from "react-toastify"
 
-const studentProfileInfo = axios.get("http://localhost:8111/student/profile", {
+const MyProfile: React.FC = () => {
+  /* if(typeof window !== "undefined") {
+  localStorage.setItem('myCat', 'Tom');
+} */
+
+  /* const studentProfileInfo = axios.get("http://localhost:8111/student/profile", {
   headers: {
       "Authorization": "bearer" + localStorage.getItem("studentAccessToken")
   },
   withCredentials: true
-})
+}) */
 
-const MyProfile: React.FC = () => {
-
-  const {data, isLoading, isError} = useQuery(["studentProfile"], () => studentProfileInfo);
+  /* const {data, isLoading, isError} = useQuery(["studentProfile"], () => studentProfileInfo);
 
   if (isLoading) {
     return <FallbackLoader />;
@@ -21,27 +27,17 @@ const MyProfile: React.FC = () => {
     return <FallbackRender error="Nastala chyba" />;
   }
 
-  console.log(data);
+  console.log(data); */
+
+  const logoutStudent = () => {
+    return;
+  }
 
   return (
     <>
       <div className="p-16">
         <div className="p-8 bg-white shadow mt-24">
           <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-              <div>
-                <p className="font-bold text-gray-700 text-xl">22</p>
-                <p className="text-gray-400">Friends</p>
-              </div>
-              <div>
-                <p className="font-bold text-gray-700 text-xl">10</p>
-                <p className="text-gray-400">Photos</p>
-              </div>
-              <div>
-                <p className="font-bold text-gray-700 text-xl">89</p>
-                <p className="text-gray-400">Comments</p>
-              </div>
-            </div>
             <div className="relative">
               <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
                 <svg
@@ -61,10 +57,10 @@ const MyProfile: React.FC = () => {
 
             <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
               <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                Connect
+                Moje požičané knihy
               </button>
-              <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                Message
+              <button onClick={logoutStudent} className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                Odlhásenie
               </button>
             </div>
           </div>
@@ -80,18 +76,6 @@ const MyProfile: React.FC = () => {
               Solution Manager - Creative Tim Officer
             </p>
             <p className="mt-2 text-gray-500">University of Computer Science</p>
-          </div>
-
-          <div className="mt-12 flex flex-col justify-center">
-            <p className="text-gray-600 text-center font-light lg:px-16">
-              An artist of considerable range, Ryan — the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-              and records all of his own music, giving it a warm, intimate feel
-              with a solid groove structure. An artist of considerable range.
-            </p>
-            <button className="text-indigo-500 py-2 px-4  font-medium mt-4">
-              Show more
-            </button>
           </div>
         </div>
       </div>

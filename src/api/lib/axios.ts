c21148880d6e250258f192b95b63d7d1b74a1f7e
Peterdinis <peterdinis0@pobox.sk>
2023-02-12 20:26:08 +0100

    feat: Create custom axios

---
+++ src/api/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useAuthStore } from "../../store/authStore";
+
+const baseEnv = process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
+
+const authApi = axios.create({
+    url: baseEnv,
+    withCredentials: true,
+});
+
+authApi.interceptors.request.use((config:any) => {
+    const token = useAuthStore.getState().token;
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    return config;
+  });
+  
+  export default authApi;

e38c9c3d9bfa4b07a198126e52ff054bc46a5c57
Peterdinis <peterdinis0@pobox.sk>
2023-02-14 11:51:32 +0100

    feat: Big auth refactor wip

--- src/store/basicStore.ts
+++
@@ -1,6 +0,0 @@
-import { mountStoreDevtool } from 'simple-zustand-devtools'
-import { useStudentStore } from './studentStore';
-
-if (process.env.NODE_ENV === 'development') {
-    mountStoreDevtool('Students', useStudentStore);
-  }

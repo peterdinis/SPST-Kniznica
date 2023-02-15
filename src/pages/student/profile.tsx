9b77aea1520204fd3c43da865050160e629b2778
Peterdinis <peterdinis0@pobox.sk>
2023-02-15 12:38:43 +0100

    feat: Using next/dynamic

--- src/pages/student/profile.tsx
+++ src/pages/student/profile.tsx
@@ -1,9 +1,14 @@
 import MyProfile from "@/components/student/MyProfile";
 import { NextPage } from "next";
+import dynamic from "next/dynamic";
+
+const DynamicProfile = dynamic(() => import("@/components/student/MyProfile"), {
+    ssr: false
+});
 
 const ProfileStudentPage: NextPage = () => {
     return (
-        <MyProfile />
+       <DynamicProfile />
     )
 }
 

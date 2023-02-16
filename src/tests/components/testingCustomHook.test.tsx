3e68491d206c6859fa334dd22d1f28ebfbe84e52
Peterdinis <peterdinis0@pobox.sk>
2023-02-16 14:53:19 +0100

    feat: Updating tests

--- src/tests/components/testingCustomHook.test.tsx
+++ src/tests/components/testingCustomHook.test.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useCustomHook } from "../mocks/customHook";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -13,8 +13,10 @@ const wrapper = ({ children }: IProps) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
 
-const { result, waitFor }: any = renderHook(() => useCustomHook(), { wrapper });
+const { result} = renderHook(() => useCustomHook(), { wrapper });
 
 waitFor(() => result.current.isSuccess);
 
+
+/* TODO: Exepct return undefined */
 expect(result.current.data).toEqual("Hello");

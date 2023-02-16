6bc22a77e215a01daf89cbf6c4cec6fa786a658b
Peterdinis <peterdinis0@pobox.sk>
2023-02-16 11:50:17 +0100

    feat: Create protected route and add 401 page

---
+++ src/routes/student/ProtectedComponent.tsx
@@ -0,0 +1,27 @@
+import Router from 'next/router';
+
+const withAuth = (WrappedComponent: any) => {
+    const WithAuth = (props: any) => {
+
+        const studentEmail = localStorage.getItem('studentEmail')
+    
+        if (studentEmail === null || studentEmail === undefined || !studentEmail) {
+            Router.push('/notallowed');
+            return null;
+        }
+    
+        return <WrappedComponent {...props} />;
+    };
+    
+    WithAuth.getInitialProps = async (ctx: any) => {
+        const wrappedComponentInitialProps = WrappedComponent.getInitialProps
+            ? await WrappedComponent.getInitialProps(ctx)
+            : {};
+    
+           return { ...wrappedComponentInitialProps };
+        };
+    
+       return WithAuth;
+    };
+    
+    export default withAuth;

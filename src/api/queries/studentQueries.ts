6a035c0c99f3f0a3933e86f5c2595ffa0b5717ac
Peterdinis <peterdinis0@pobox.sk>
2023-02-20 22:53:40 +0100

    feat: Remove code

--- src/api/queries/studentQueries.ts
+++ src/api/queries/studentQueries.ts
@@ -4,14 +4,6 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
 });
 
-api.interceptors.request.use((config: any) => {
-  const token = localStorage.getItem("studentAccessToken");
-  config.headers = {
-    Authorization: `Bearer ${token}`,
-  };
-  return config;
-});
-
 export const studentProfile = api
   .get("student/profile")
   .then((res) => res.data);
--- src/components/student/MyProfile.tsx
+++ src/components/student/MyProfile.tsx
@@ -1,21 +1,16 @@
-import ProtectedRoute from "@/routes/student/ProtectedRoute";
 import ProfileHeader from "./profile/ProfileHeader";
 import ProfileBody from "./profile/ProfileBody";
 
 const MyProfile: React.FC = () => {
-  const email = localStorage.getItem("studentEmail");
-  if(!email) {
-    window.location.replace("/notallowed");
-  }
   return (
-    <ProtectedRoute>
+    <>
       <div className="container mx-auto my-5 p-5">
         <div className="md:flex no-wrap md:-mx-2">
           <ProfileHeader />
           <ProfileBody />
         </div>
       </div>
-    </ProtectedRoute>
+    </>
   );
 };
 

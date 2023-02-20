07e2f6b5a311d8737a99bd8ef3af4bab90fbca6a
Peterdinis <peterdinis0@pobox.sk>
2023-02-20 14:50:29 +0100

    style: Fix some style problems

--- src/components/category/CreateNewCategory.tsx
+++ src/components/category/CreateNewCategory.tsx
@@ -1,16 +1,19 @@
 import Header from "../shared/Header";
 import { useMutation } from "@tanstack/react-query";
 import * as mut from "../../api/mutations/categoryMutation";
-import { TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import BaseButton from "../shared/BaseButton";
+import {toast} from "react-toastify";
 
 type FormData = {
   name: string;
   description: string;
 };
+
+const notify = () => toast.success("Kategória bola vytvorená");
+const errorRegister = () => toast.error("Kategória nebola vytvorená");
 
 const schema = yup
   .object({
@@ -24,6 +27,7 @@ const CreateNewCategory: React.FC = () => {
     resolver: yupResolver(schema),
   });
 
+  /* TODO: Later update this logic */
   const mutation = useMutation(mut.createNewCategory);
 
   return (
@@ -33,37 +37,38 @@ const CreateNewCategory: React.FC = () => {
         <form
           onSubmit={handleSubmit((data: FormData) => {
             mutation.mutate(data);
+            notify();
           })}
         >
           <div className="relative z-0 mb-6 group">
             <input
-              type="email"
-              name="floating_email"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
+              type="text"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              {...register("name", {
+                required: true
+              })}
             />
             <label
-              htmlFor="floating_email"
-              className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="name"
+              className="absolute text-sm text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Email address
+              Meno kategórie
             </label>
           </div>
           <br />
           <div className="relative z-0 mb-6 group">
             <input
-              type="email"
-              name="floating_email"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required
+              type="text"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              {...register("description", {
+                required: true
+              })}
             />
             <label
-              htmlFor="floating_email"
-              className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="description"
+              className="absolute text-sm text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Email address
+              Popis kategórie
             </label>
           </div>
           <BaseButton name="Vytvor novú kategóriu" />

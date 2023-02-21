423bf4b7e62c00fd0cd7a23533d149bd839a26ec
Peterdinis <peterdinis0@pobox.sk>
2023-02-21 20:22:16 +0100

    FOO

---
+++ cypress/e2e/books.cy.ts
@@ -0,0 +1,17 @@
+describe("Go to books page", () => {
+    it("Visit book page", () => {
+        cy.visit("http://localhost:3000/books/all")
+    })
+})
+
+describe("Go to books search page", () => {
+    it("Visit book search page", () => {
+        return;
+    })
+})
+
+describe("Go to books detail page", () => {
+    it("Go to books detail page", () => {
+        return;
+    })
+})

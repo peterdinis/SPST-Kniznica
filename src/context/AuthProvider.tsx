1f3b1df9ab98e59cfe4bcf573baccef571f02ac3
Peterdinis <peterdinis0@pobox.sk>
2023-02-24 11:11:40 +0100

    feat: WRiting auth context wip

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -5,4 +5,51 @@ import React, {
   useContext,
   createContext,
 } from "react";
-import {auth} from "../firebase/init";
+import { auth } from "../firebase/init";
+import {
+  Auth,
+  UserCredential,
+  User,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  confirmPasswordReset,
+} from "firebase/auth";
+
+export interface AuthProviderProps {
+  children?: ReactNode;
+}
+
+export interface UserContextState {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  id?: string;
+}
+
+export const UserStateContext = createContext<UserContextState>(
+  {} as UserContextState
+);
+
+export interface AuthContextModel {
+  auth: Auth;
+  user: User | null;
+  signIn: (email: string, password: string) => Promise<UserCredential>;
+  signUp: (email: string, password: string) => Promise<UserCredential>;
+  sendPasswordResetEmail?: (email: string) => Promise<void>;
+  signInWithGoogle: () => Promise<void>;
+  forgotPassword: () => Promise<void>;
+  resetPassword: () => Promise<void>;
+}
+
+export const AuthContext = React.createContext<AuthContextModel>(
+    {} as AuthContextModel,
+  )
+  
+  export function useAuth(): AuthContextModel {
+    return useContext(AuthContext)
+  }
+  

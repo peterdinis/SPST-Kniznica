d3e12f369973e31429641c03ac8585f35d102748
Peterdinis <peterdinis0@pobox.sk>
2023-02-24 17:04:40 +0100

    foo

--- src/components/teacher/profile/ProfileHeader.tsx
+++ src/components/teacher/profile/ProfileHeader.tsx
@@ -1,19 +1,32 @@
 import { useAuth } from "@/context/AuthProvider";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const ProfileHeader: React.FC = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
+  const router = useRouter();
 
-  const userPhoto = user?.photoURL || "https://picsum.photos/200/300"
+  useEffect(() => {
+    if (user) {
+      console.log("signed in!");
+    } else if (user == null) {
+      router.push("/student/login");
+    }
+  }, [user]);
+
+  if (!user) {
+    // user is signed out or still being checked.
+    // don't render anything
+    return null;
+  }
+
+  const userPhoto = user?.photoURL || "https://picsum.photos/200/300";
 
   return (
     <div className="w-full mt-20 md:w-3/12 md:mx-2">
       <div className="bg-white p-2">
         <div className="image overflow-hidden">
-          <img
-            className="h-auto w-full mx-auto"
-            src={userPhoto}
-            alt="IMAGES"
-          />
+          <img className="h-auto w-full mx-auto" src={userPhoto} alt="IMAGES" />
         </div>
         <h1 className="text-gray-900 font-bold text-xl leading-8 my-1">
           {user?.email}

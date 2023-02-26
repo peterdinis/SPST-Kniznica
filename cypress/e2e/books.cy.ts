52e0203dbc606c53a7c22f819fa764aeea1fea2f
Peterdinis <peterdinis0@pobox.sk>
2023-02-26 11:33:09 +0100

    feat: Create basic tests wip

--- cypress/e2e/books.cy.ts
+++ cypress/e2e/books.cy.ts
@@ -9,3 +9,12 @@ describe("Go to books search page", () => {
         cy.visit("http://localhost:3000/books/search")
     })
 })
+
+
+describe("Go to book detail page", () => {
+    it("Visit book detail page", () => {
+        cy.visit("http://localhost:3000/books/all")
+        cy.contains('Detail').click()
+    })
+})
+

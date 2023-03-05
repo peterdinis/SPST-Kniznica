e953ba832287633b94b6d5ac83fb36142e13ba0c
Peterdinis <peterdinis0@pobox.sk>
2023-03-05 12:19:50 +0100

    FOO

---
+++ src/api/queries/adminQueries.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseEnv = process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
+
+const api = axios.create({
+  baseURL: baseEnv
+});
+
+
+export const getExampleData = () => api.get("admin/example").then((res) => res.data);

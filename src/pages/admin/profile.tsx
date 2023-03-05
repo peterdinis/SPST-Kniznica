0c0abb46ff1ff3e0b9bca16288fc52c21c296195
Peterdinis <peterdinis0@pobox.sk>
2023-03-05 16:01:06 +0100

    feat: Updating auth forms

---
+++ src/pages/admin/profile.tsx
@@ -0,0 +1,10 @@
+import ProfilePage from "@/components/admin/ProfilePage";
+import { NextPage } from "next";
+
+const AdminProfilePage: NextPage = () => {
+    return (
+       <ProfilePage />
+    )
+}
+
+export default AdminProfilePage;
---
+++ src/pages/admin/register.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/components/admin/RegisterForm";
+import { NextPage } from "next";
+
+const AdminRegisterPage: NextPage = () => {
+    return (
+        <RegisterForm />
+    )
+}
+
+export default AdminRegisterPage;

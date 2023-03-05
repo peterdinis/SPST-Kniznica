28bed9d096003dfcb0d36b1a2b2f7c0096b848b1
Peterdinis <peterdinis0@pobox.sk>
2023-03-05 14:59:42 +0100

    feat: Start working on teacher auth

--- src/interfaces/ITeacher.ts
+++ src/interfaces/ITeacher.ts
@@ -1,14 +1,33 @@
 export interface IRegister {
-    name: string;
-    lastName: string;
-    username: string;
-    email: string;
-    password: string;
-    role: string;
+  name: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  classRoom: string;
+}
+
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
+
+export interface ILoginTeacherInfo {
+  data: {
+    token: string;
+    user: {
+      id: number;
+      name: string;
+      createdAt: string;
+      classRoom: string;
+      email: string;
+      lastName: string;
+      password: string;
+      role: string;
+      updatedAt: string;
+      username: string;
+    }
   }
-  
-  export interface ILogin {
-    email: string;
-    password: string;
-  }
-  
+}

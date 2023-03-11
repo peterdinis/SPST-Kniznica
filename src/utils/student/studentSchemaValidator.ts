d47d258965673b1b4a3eaf4fe6d9ec75bc4b6019
Peterdinis <peterdinis0@pobox.sk>
2023-03-11 09:55:26 +0100

    feat: Finish validator for study

--- src/utils/student/studentSchemaValidator.ts
+++ src/utils/student/studentSchemaValidator.ts
@@ -2,16 +2,16 @@ import { z } from "zod";
 
 export const registerStudentSchema = z
   .object({
+    name: z.string().min(1, "Name is required").max(30),
+    lastName: z.string().min(1, "LastName is required").max(100),
     username: z.string().min(1, "Username is required").max(100),
     email: z.string().email("Invalid email").min(1, "Email is required"),
     password: z
       .string()
       .min(1, "Password is required")
       .min(8, "Password must have more than 8 characters"),
-    confirmPassword: z.string().min(1, "Password confirmation is required"),
-    terms: z.literal(true, {
-      errorMap: () => ({ message: "You must accept the terms and conditions" }),
-    }),
+    role: z.string().min(1, "Role is required").max(30),
+    classRoom: z.string().min(1, "Classroom is required").max(30)
 });
 
 export const loginStudentSchem = z.object({

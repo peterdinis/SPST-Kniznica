7d406a97a202c559c8c3ae17d9788bceb98ad8ae
Peterdinis <peterdinis0@pobox.sk>
2023-03-11 11:22:12 +0100

    FOO

--- src/utils/booking/createBookingSchemaValidator.ts
+++ src/utils/booking/createBookingSchemaValidator.ts
@@ -1,3 +1,32 @@
-import { z } from "zod";
+import {z} from "zod";
 
-export const createBookingSchema = z.object({});
+export const createBookingSchema = z.object({
+    from: z.string({
+        required_error: "From is required"
+    }),
+
+    to: z.string({
+        required_error: "To is required"
+    }),
+
+    username: z.string({
+        required_error: "Username is required"
+    }),
+
+    bookId: z.number({
+        required_error: "Must be integer"
+    })
+});
+
+export const returnBookingSchema = z.object({
+    username: z.string({
+        required_error: "Username is required"
+    }),
+
+    bookId: z.number({
+        required_error: "Must be integer"
+    })
+});
+
+export type createBookingType = z.infer<typeof createBookingSchema>;
+export type returnBookingType = z.infer<typeof returnBookingSchema>;

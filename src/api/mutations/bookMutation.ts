790c528ed1eb53e830bf95249d8d27e0745de4e3
Peterdinis <peterdinis0@pobox.sk>
2023-03-18 10:27:22 +0100

    FOO

--- src/api/mutations/bookMutation.ts
+++ src/api/mutations/bookMutation.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { IBook } from "../../interfaces/IBook";
 
-const baseEnv = process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
+const baseEnv =
+  process.env.NODE_ENV !== "production"
+    ? (process.env.NEXT_PUBLIC_BACKEND_URL as string)
+    : (process.env.NEXT_PUBLIC_PRODUCTION_URL as string);
 
 const api = axios.create({
-  baseURL: baseEnv
+  baseURL: baseEnv,
 });
 
-
 export const createNewBook = (data: IBook) => {
-    return api.post("books", data);
-}
+  return api.post("books", data);
+};
--- src/api/mutations/bookingMutations.ts
+++ src/api/mutations/bookingMutations.ts
@@ -1,17 +1,19 @@
 import { IBooking } from "@/interfaces/IBooking";
 import axios from "axios";
 
-const baseEnv = process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
+const baseEnv =
+  process.env.NODE_ENV !== "production"
+    ? (process.env.NEXT_PUBLIC_BACKEND_URL as string)
+    : (process.env.NEXT_PUBLIC_PRODUCTION_URL as string);
 
 const api = axios.create({
-  baseURL: baseEnv
+  baseURL: baseEnv,
 });
-
 
 export const createNewBooking = (data: IBooking) => {
   return api.post("booking", data);
-}
+};
 
 export const returnBooking = () => {
   return;
-}
+};

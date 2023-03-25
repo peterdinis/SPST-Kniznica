bbab6e31c14e6d07fc1c38d8fb06bd703eb19487
Peterdinis <peterdinis0@pobox.sk>
2023-03-25 13:43:42 +0100

    feat: Update about page

---
+++ src/components/about/Informations.tsx
@@ -0,0 +1,13 @@
+import BasicInformation from "../shared/BasicInformations";
+import Header from "../shared/Header";
+
+const Informations: React.FC = () => {
+    return (
+        <>
+         <Header name="Základné informácie" />
+         <BasicInformation />
+        </>
+    )
+}
+
+export default Informations;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,14 @@
+import Informations from "@/components/about/Informations";
 import AboutWrapper from "../components/about/AboutWrapper";
 import { NextPage } from "next";
 
 const AboutPage: NextPage = () => {
-  return <AboutWrapper />;
+  return (
+    <>
+      <AboutWrapper />
+      <Informations />
+    </>
+  )
 };
 
 export default AboutPage;

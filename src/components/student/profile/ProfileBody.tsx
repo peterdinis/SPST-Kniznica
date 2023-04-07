4514647293b0ae8449a34964826da05105b62056
Peterdinis <peterdinis0@pobox.sk>
2023-04-07 11:51:19 +0200

    feat: Start to updating booking in frontend wip

--- src/components/student/profile/ProfileBody.tsx
+++ src/components/student/profile/ProfileBody.tsx
@@ -65,20 +65,6 @@ const ProfileBody: React.FC = () => {
                   </button>
                 </div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">
-                  Moje požičané knihy
-                </div>
-                <div className="px-4 py-2">
-                  {" "}
-                  <Link
-                    className="font-bod text-red-800"
-                    href={`/booking/student/${studentUsername}`}
-                  >
-                    Moje knihy
-                  </Link>
-                </div>
-              </div>
             </div>
           </div>
         </div>

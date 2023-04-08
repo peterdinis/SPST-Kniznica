2bf1395bff2b3570bb544de52a7bf4267af8d1f1
Peterdinis <peterdinis0@pobox.sk>
2023-04-08 20:17:13 +0200

    refactor: Change lib for token

--- src/api/queries/studentQueries.ts
+++ src/api/queries/studentQueries.ts
@@ -1,4 +1,4 @@
-import authApi from "../lib/studentInterceptor";
+import authApi from "../tokens/studentInterceptor";
 
 export const getStudentProfile = () =>
   authApi.get("student/profile").then((res) => res.data);
--- src/api/queries/teacherQueries.ts
+++ src/api/queries/teacherQueries.ts
@@ -1,3 +1,3 @@
-import authApi from "../lib/studentInterceptor";
+import authApi from "../tokens/studentInterceptor";
 
 export const getTeacherProfile = () => authApi.get("teacher/profile").then((res) => res.data);

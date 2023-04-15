3aa50a73abe27e05392f238a1f6fc7ede9dbbe56
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 16:35:20 +0200

    fix: Adding react-error-boundary to ErrorRender component

--- src/api/queries/adminQueries.ts
+++ src/api/queries/adminQueries.ts
@@ -3,3 +3,5 @@ import adminApi from "../tokens/adminInterceptor";
 export const getAdminExampleData = () => adminApi.get("admin/example").then((res) => res.data);
 
 export const getAdminMessages = () => adminApi.get("admin/messages").then((res) => res.data);
+
+export const getAllNotifications = () => adminApi.get("notifications").then((res) => res.data);
--- src/components/shared/errors/ErrorRender.tsx
+++ src/components/shared/errors/ErrorRender.tsx
@@ -1,9 +1,28 @@
+import { Button } from '@mui/material'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
+import { ErrorBoundary } from 'react-error-boundary'
+
 interface IProps {
   error: string;
+  children?: React.ReactNode;
 }
 
-function FallbackRender({ error }: IProps) {
-  return <p className="mt-4 text-2xl font-bold">{error}</p>;
-}
+const FallbackRender: React.FC<IProps> = ({error, children}: IProps) => (
+  <QueryErrorResetBoundary>
+    {({ reset }) => (
+      <ErrorBoundary
+        onReset={reset}
+        fallbackRender={({ resetErrorBoundary }) => (
+          <div>
+            {error}
+            <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+          </div>
+        )}
+      >
+        {children}
+      </ErrorBoundary>
+    )}
+  </QueryErrorResetBoundary>
+)
 
-export default FallbackRender;
+export default FallbackRender 

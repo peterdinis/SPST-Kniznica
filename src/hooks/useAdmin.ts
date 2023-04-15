4916856ddd36a986f00060c4d39d4704f10fa61a
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 18:32:01 +0200

    FOO

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -1,5 +1,20 @@
+import Cookies from "js-cookie";
+import {useState, useEffect} from "react";
+
 const useAdmin = () => {
-    
+    // TODO: Remove any later
+    const [admin, setAdmin] = useState<any | null>(null);
+
+    useEffect(() => {
+        const currentAdmin = Cookies.get("adminData");
+        if (currentAdmin) {
+            setAdmin(JSON.parse(currentAdmin));
+        }
+    }, []);
+
+    return {
+        admin,
+    }
 }
 
 export default useAdmin;

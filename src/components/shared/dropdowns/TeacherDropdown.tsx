ce2674fce2df6ead23f3d3c54888a899a497d42e
Peterdinis <peterdinis0@pobox.sk>
2023-04-17 19:14:03 +0200

    refactor: Move code for teacher notification to right component

--- src/components/shared/dropdowns/TeacherDropdown.tsx
+++ src/components/shared/dropdowns/TeacherDropdown.tsx
@@ -2,18 +2,59 @@ import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Fragment } from "react";
 import classNames from "@/helpers/classNames";
+import { useQuery } from "@tanstack/react-query";
+import * as api from "../../../api/queries/teacherQueries";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import { placeholderBooking } from "@/data/placeholderBooking";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import { ILoginTeacherInfo } from "@/interfaces/ITeacher";
+import FallbackLoader from "@/components/shared/FallbackLoader";
+import FallbackRender from "@/components/shared/errors/ErrorRender";
+import { IBooking } from "@/interfaces/IBooking";
 
 const TeacherDropdown: React.FC = () => {
+  const [teacher, setTeacher] = useState<ILoginTeacherInfo | null>(null);
+
+  useEffect(() => {
+    const currentTeacher = Cookies.get("teacherData");
+    if (currentTeacher) {
+      setTeacher(JSON.parse(currentTeacher));
+    }
+  }, []);
+
+  const teacherUsername = teacher?.data.user.username;
+
+  const { data, isError, isLoading } = useQuery(
+    ["teacherNotifications", teacherUsername],
+    () => api.getMyNotifications(teacherUsername as unknown as string),
+    {
+      initialData: placeholderBooking,
+      retry: 2,
+    }
+  );
+
+  if (isError) {
+    return <FallbackRender error="Nastala chyba" />;
+  }
+
+  if (isLoading) {
+    return <FallbackLoader />;
+  }
+
+  const notificationNum = data.length as unknown as number;
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Options
-          <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </Menu.Button>
+      <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <NotificationsActiveIcon /> {notificationNum}
+        <ChevronDownIcon
+          className="-mr-1 h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+      </Menu.Button>
       </div>
 
       <Transition
--- src/components/teacher/profile/MyBooks.tsx
+++ src/components/teacher/profile/MyBooks.tsx
@@ -24,23 +24,6 @@ const MyBooks: React.FC = () => {
 
   const teacherUsername = teacher?.data.user.username;
 
-  const { data, isError, isLoading } = useQuery(
-    ["teacherBorrowedBooks", teacherUsername],
-    () => api.getMyNotifications(teacherUsername as unknown as string),
-    {
-      initialData: placeholderBooking,
-      retry: 2,
-    }
-  );
-
-  if (isError) {
-    return <FallbackRender error="Nastala chyba" />;
-  }
-
-  if (isLoading) {
-    return <FallbackLoader />;
-  }
-
   return (
     <>
       <div className="w-full md:w-9/12 mx-2 h-64">
@@ -49,38 +32,7 @@ const MyBooks: React.FC = () => {
             <MenuBookIcon />
             <span className="tracking-wide">Moje požičané knihy</span>
           </div>
-          {data === null ||
-            data === undefined ||
-            (!data && (
-              <div className="font-bold text-lg">
-                Učiteľ nemá požičané žiadne knihy{" "}
-                <SentimentVeryDissatisfiedIcon />
-              </div>
-            ))}
-          {data &&
-            data.map((item: IBooking) => {
-              return (
-                <>
-                  <div className="py-3 text-sm">
-                    <div className="flex justify-start cursor-pointer text-gray-700 rounded-md px-2 py-2 my-2">
-                      <span className="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
-                      <div className="flex-grow font-medium px-2">
-                        <Link href={`/books/detail/${item.bookId}`}>
-                          Detail Knihy - {item.bookId}
-                        </Link>
-                      </div>
-                      <div className="text-sm font-normal text-gray-500 tracking-wide">
-                        {item.from}
-                      </div>
-                      <div className="text-sm ml-10 font-normal text-gray-500 tracking-wide">
-                        {item.to}
-                      </div>
-                      <ReturnBookModal />
-                    </div>
-                  </div>
-                </>
-              );
-            })}
+          TEXT
         </div>
       </div>
     </>

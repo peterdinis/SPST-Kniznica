f0281f83b6cefea95e1d475b044ff10ad5ac86d9
Peterdinis <peterdinis0@pobox.sk>
2023-04-21 19:18:06 +0200

    feat: Remove some pages

--- src/pages/adminallowed.tsx
+++
@@ -1,8 +0,0 @@
-import AdminComponent from "@/components/shared/AdminComponent";
-import { NextPage } from "next";
-
-const AdminAllowedPage: NextPage = () => {
-  return <AdminComponent />
-};
-
-export default AdminAllowedPage;

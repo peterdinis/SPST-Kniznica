076c956b76d06e67a85d6d3d0e6d98f0bc6ad08b
Peterdinis <peterdinis0@pobox.sk>
2023-04-28 19:48:16 +0200

    feat: Cleanup frontend

--- src/api/queries/studentQueries.ts
+++ src/api/queries/studentQueries.ts
@@ -2,8 +2,3 @@ import authApi from "../tokens/studentInterceptor";
 
 export const getStudentProfile = () =>
   authApi.get("student/profile").then((res) => res.data);
-
-export const getMyNotifications = (username: string) => {
-  if(!username) return;
-  return authApi.get(`student/${username}/notifications`).then((res) => res.data);
-}

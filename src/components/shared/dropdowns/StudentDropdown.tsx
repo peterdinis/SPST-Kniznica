89e954c759d6d1fd2b0f70372dd99631b027e030
Peterdinis <peterdinis0@pobox.sk>
2023-04-28 19:53:10 +0200

    feat: Cleaning code in frontend wip

--- src/components/shared/dropdowns/StudentDropdown.tsx
+++ src/components/shared/dropdowns/StudentDropdown.tsx
@@ -3,44 +3,24 @@ import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Fragment, useEffect, useState } from "react";
 import classNames from "@/helpers/classNames";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
-import { useQuery } from "@tanstack/react-query";
-import * as api from "../../../api/queries/studentQueries";
-import FallbackLoader from "../FallbackLoader";
-import FallbackRender from "../errors/ErrorRender"
 import { ILoginStudentInfo } from "@/interfaces/IStudent";
 import Cookies from "js-cookie";
-import { placeholderNotification } from "@/data/placeholderNotification";
 
 const StudentDropdown: React.FC = () => {
   const [student, setStudent] = useState<ILoginStudentInfo | null>(null);
   useEffect(() => {
-      const currentStudent = Cookies.get("studentData");
-      if (currentStudent) {
-          setStudent(JSON.parse(currentStudent));
-      }
+    const currentStudent = Cookies.get("studentData");
+    if (currentStudent) {
+      setStudent(JSON.parse(currentStudent));
+    }
   }, []);
-  
+
   const studentUsername = student?.data.user.username as unknown as string;
-  const {data, isLoading, isError} = useQuery(["studentNotification", studentUsername], () => api.getMyNotifications(studentUsername), {
-    placeholderData: placeholderNotification,
-    retry: 2
-  })
 
-
-  if (isError) {
-    return <FallbackRender error="Nastala chyba" />;
-  }
-
-  if (isLoading) {
-    return <FallbackLoader />;
-  }
-
-  const notificationNum = data.length as unknown as number;
-  
   return (
     <Menu as="div" className="relative inline-block text-left">
       <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        <NotificationsActiveIcon /> {notificationNum}
+        <NotificationsActiveIcon /> 0
         <ChevronDownIcon
           className="-mr-1 h-5 w-5 text-gray-400"
           aria-hidden="true"
@@ -56,24 +36,7 @@ const StudentDropdown: React.FC = () => {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          {notificationNum === 0 ? (
-           <div className="py-1">
-           <Menu.Item>
-             {({ active }) => (
-               <a
-                 href="#"
-                 className={classNames(
-                   active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                   "block px-4 py-2 text-sm"
-                 )}
-               >
-                 Žiadne upozornenia
-               </a>
-             )}
-           </Menu.Item>
-         </div>
-          ): (
-            <div className="py-1">
+          <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <a
@@ -83,12 +46,11 @@ const StudentDropdown: React.FC = () => {
                     "block px-4 py-2 text-sm"
                   )}
                 >
-                  Random Message
+                  Žiadne upozornenia
                 </a>
               )}
             </Menu.Item>
           </div>
-          )}
         </Menu.Items>
       </Transition>
     </Menu>
--- src/components/shared/dropdowns/TeacherDropdown.tsx
+++ src/components/shared/dropdowns/TeacherDropdown.tsx
@@ -4,7 +4,6 @@ import { Fragment } from "react";
 import classNames from "@/helpers/classNames";
 import { useQuery } from "@tanstack/react-query";
 import * as api from "../../../api/queries/teacherQueries";
-import Link from "next/link";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import { placeholderBooking } from "@/data/placeholderBooking";
@@ -12,6 +11,7 @@ import { ILoginTeacherInfo } from "@/interfaces/ITeacher";
 import FallbackLoader from "@/components/shared/FallbackLoader";
 import FallbackRender from "@/components/shared/errors/ErrorRender";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+
 const TeacherDropdown: React.FC = () => {
   const [teacher, setTeacher] = useState<ILoginTeacherInfo | null>(null);
 
@@ -24,35 +24,16 @@ const TeacherDropdown: React.FC = () => {
 
   const teacherUsername = teacher?.data.user.username;
 
-  const { data, isError, isLoading } = useQuery(
-    ["teacherNotifications", teacherUsername],
-    () => api.getMyNotifications(teacherUsername as unknown as string),
-    {
-      initialData: placeholderBooking,
-      retry: 2,
-    }
-  );
-
-  if (isError) {
-    return <FallbackRender error="Nastala chyba" />;
-  }
-
-  if (isLoading) {
-    return <FallbackLoader />;
-  }
-
-  const notificationNum = data.length as unknown as number;
-
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-      <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        <NotificationsActiveIcon /> {notificationNum}
-        <ChevronDownIcon
-          className="-mr-1 h-5 w-5 text-gray-400"
-          aria-hidden="true"
-        />
-      </Menu.Button>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          <NotificationsActiveIcon /> 0
+          <ChevronDownIcon
+            className="-mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        </Menu.Button>
       </div>
 
       <Transition

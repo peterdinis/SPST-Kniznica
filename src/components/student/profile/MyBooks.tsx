f5fe0300871cb39d75768755ed5a85a0cf08bbf9
Peterdinis <peterdinis0@pobox.sk>
2023-05-05 19:57:13 +0200

    fix: Fixing problems in component

--- src/components/student/profile/MyBooks.tsx
+++ src/components/student/profile/MyBooks.tsx
@@ -1,8 +1,32 @@
 import useStudent from "@/hooks/useStudent";
+import * as api from "@/api/queries/bookingQueries";
+import {useQuery} from "@tanstack/react-query";
+import FallbackLoader from "@/components/shared/FallbackLoader";
+import FallbackRender from "@/components/shared/errors/ErrorRender";
+import { useRouter } from "next/router";
 
 const MyBooks: React.FC = () => {
   const { studentPersonalInfo } = useStudent();
+  const router = useRouter();
+  const studentUserName = studentPersonalInfo?.username as unknown as string;
+  const queryUserName = router.query.username![0] as unknown as string;
 
+  const wrapperName = studentUserName === undefined || studentUserName === null ? queryUserName : studentUserName;
+
+  console.log(wrapperName);
+
+  /* console.log(studentPersonalInfo && studentUserName);
+  const {data, isLoading, isError} = useQuery(["myBorrowedBooks", studentUserName], () => api.getMyBorrowedBooks(studentPersonalInfo && studentUserName as any))
+
+ if(isLoading) {
+    return <FallbackLoader />
+  }
+
+  if(isError) {
+    return <FallbackRender error="Nastala chyba" />
+  }
+
+  console.log(data); */
   return (
     <section className="container mx-auto p-6 font-mono">
     <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
@@ -35,158 +59,6 @@ const MyBooks: React.FC = () => {
                 <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
               </td>
               <td className="px-4 py-3 text-sm border">6/4/2000</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold text-black">Stevens</p>
-                    <p className="text-xs text-gray-600">Programmer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 text-md font-semibold border">27</td>
-              <td className="px-4 py-3 text-xs border">
-                <span className="px-2 py-1 font-semibold leading-tight text-orange-700 bg-gray-100 rounded-sm"> Pending </span>
-              </td>
-              <td className="px-4 py-3 text-sm border">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Nora</p>
-                    <p className="text-xs text-gray-600">Designer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 text-md font-semibold border">17</td>
-              <td className="px-4 py-3 text-xs border">
-                <span className="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-sm"> Nnacceptable </span>
-              </td>
-              <td className="px-4 py-3 text-sm border">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Ali</p>
-                    <p className="text-xs text-gray-600">Programmer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">23</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Khalid</p>
-                    <p className="text-xs text-gray-600">Designer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">20</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-gray-700 bg-gray-100 rounded-sm"> Pending </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Nasser</p>
-                    <p className="text-xs text-gray-600">Pen Tester</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">29</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Mohammed</p>
-                    <p className="text-xs text-gray-600">Web Designer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">38</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Saad</p>
-                    <p className="text-xs text-gray-600">Data</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">19</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
-            </tr>
-            <tr className="text-gray-700">
-              <td className="px-4 py-3 border">
-                <div className="flex items-center text-sm">
-                  <div className="relative w-8 h-8 mr-3 rounded-full">
-                    <img className="object-cover w-full h-full rounded-full" src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" loading="lazy" />
-                    <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                  </div>
-                  <div>
-                    <p className="font-semibold">Sami</p>
-                    <p className="text-xs text-gray-600">Developer</p>
-                  </div>
-                </div>
-              </td>
-              <td className="px-4 py-3 border text-md font-semibold">21</td>
-              <td className="px-4 py-3 border text-xs">
-                <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Acceptable </span>
-              </td>
-              <td className="px-4 py-3 border text-sm">6/10/2020</td>
             </tr>
           </tbody>
         </table>

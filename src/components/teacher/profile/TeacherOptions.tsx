df758144377a44b5c441a0717d1afbeca7205a9a
Peterdinis <peterdinis0@pobox.sk>
2023-05-06 20:01:34 +0200

    feat: Update teacher options component

--- src/components/teacher/profile/TeacherOptions.tsx
+++ src/components/teacher/profile/TeacherOptions.tsx
@@ -1,38 +1,72 @@
 import SettingsIcon from "@mui/icons-material/Settings";
-import CreateIcon from "@mui/icons-material/Create";
-import ListIcon from "@mui/icons-material/List";
+import Link from "next/link";
 
 const TeacherOptions: React.FC = () => {
   return (
     <>
-      <div className="pt-6 w-full md:w-9/12 mx-2 h-64">
+      <div className="mt-10 w-full md:w-9/12 mx-2 h-64">
         <div className="bg-white p-3 shadow-sm rounded-sm">
           <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
             <SettingsIcon />
             <span className="tracking-wide">Moje možnosti</span>
           </div>
-          <div className="mt-2 p-2">
-            
-           <li>
-              <a className="text-red-800 ml-4" href="/admin/books/all">
-                <ListIcon /> Zobraz všetky knihy
-              </a>
-            </li>
-            <li>
-              <a className="text-red-800 ml-4" href="/admin/categories/all">
-                <ListIcon /> Zobraz všetky kategórie
-              </a>
-            </li>
-            <li>
-              <a className="text-red-800 ml-4" href="/books/create">
-                <CreateIcon /> Vytvor knihu
-              </a>
-            </li>
-            <li>
-              <a className="ml-4 text-red-800" href="/category/create">
-                <CreateIcon /> Vytvor kategóriu
-              </a>
-            </li>
+          <div className="text-gray-700">
+            <div className="grid md:grid-cols-2 text-sm">
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Zobraziť všetky knihy
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/admin/books/all">Všetky knihy</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Zobraziť všetky kategórie
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/admin/categories/all">Všetky kategórie</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Zobraziť všetky objednávky
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/admin/booking/all">Všetky objednávky</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">Pridať novú knihu</div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/books/create">Nová kniha</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Pridanie novej kategórie
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/category/create">Nová kategória</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Zoznam všetkých žiakov
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/admin/student/all">Zoznam žiakov</Link>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">
+                  Zoznam všetkých učiteľov
+                </div>
+                <div className="px-4 py-2">
+                  <Link className="text-red-500" href="/admin/teacher/all">Zoznam učiteľov</Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

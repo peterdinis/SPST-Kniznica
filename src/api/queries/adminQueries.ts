1a3393eb91cf6570ac825b42c53ab5b6253afd08
Peterdinis <peterdinis0@pobox.sk>
2023-05-06 13:43:48 +0200

    feat: Adding pagination for categories

--- src/api/queries/adminQueries.ts
+++ src/api/queries/adminQueries.ts
@@ -3,8 +3,5 @@ import adminApi from "../tokens/adminInterceptor";
 export const getAdminExampleData = () =>
   adminApi.get("admin/example").then((res) => res.data);
 
-export const getAdminMessages = () =>
-  adminApi.get("admin/messages").then((res) => res.data);
-
 export const getStudentProfile = () =>
   adminApi.get("admin/profile").then((res) => res.data);

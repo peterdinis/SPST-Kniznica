0098467f7b55eb38c289e125deb414efce3a869a
Peterdinis <peterdinis0@pobox.sk>
2023-05-08 13:41:46 +0200

    feat: Updating admin tables

---
+++ src/data/placeholderPaginatedBooking.ts
@@ -0,0 +1,19 @@
+export interface IPaginatedBooking {
+  data: {
+    result: {
+      0: {
+        username: "Janko Hraško";
+        from: "2023-05-08T11:38:04.975Z";
+        to: "2023-05-08T11:38:04.975Z";
+        bookId: 1;
+      };
+
+      1: {
+        username: "Hraško Janko";
+        from: "2023-05-08T11:38:04.975Z";
+        to: "2023-05-08T11:38:04.975Z";
+        bookId: 2;
+      };
+    };
+  };
+}

d7c9473c525cd3dc7ab496abd4440c60019b4230
Peterdinis <peterdinis0@pobox.sk>
2023-05-26 19:20:25 +0200

    FOO

--- src/components/books/Reasons.tsx
+++ src/components/books/Reasons.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@mui/material";
+import Mailto from "../shared/Mailto";
 
 const Reasons: React.FC = () => {
   return (
@@ -6,11 +7,7 @@ const Reasons: React.FC = () => {
       <Typography className="text-red-700 font-bold text-xl">
         Potencionálne problémy:
         <ul className="ml-4 mt-4 max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-          <li>Snažíte sa požičať knihu offline</li>
-          <li>Zadali ste zle id knihy</li>
-          <li>Zadali ste zlé používateľské meno</li>
-          <li>Do dátum od alebo do ste zadali dátum z minulosti</li>
-          <li>Ak to nebol žiaden z týchto problémov je to problém applikácie napíšte na: pdinis1@gmail.com</li>
+          <Mailto mailto="Ak to nebol žiaden z týchto problémov je to problém applikácie napíšte na:"  mailtoEmail="pdinis1@gmail.com"/>
         </ul>
       </Typography>
     </div>

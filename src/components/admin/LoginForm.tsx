c945ff3911230aa10e4f63d090faef25cf32dfb0
Peterdinis <peterdinis0@pobox.sk>
2023-05-27 11:11:21 +0200

    FOO

--- src/components/admin/LoginForm.tsx
+++ src/components/admin/LoginForm.tsx
@@ -8,13 +8,52 @@ import * as mut from "../../api/mutations/studentMutations";
 import { useRouter } from "next/router";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Cookies from "js-cookie";
+import {
+  createAdminRegisterType,
+  loginAdminSchema,
+} from "@/validators/admin/adminSchema";
 
 const LoginForm: React.FC = () => {
-    return (
-        <>
-         <Header name="Prihlásenie Admin" />
-        </>
-    )
-}
+  const router = useRouter();
+
+  const notify = () => toast.success("Prihlásenie bolo úspešné");
+  const errorRegister = () => toast.error("Prihlásenie nebolo úspešné");
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    trigger,
+    register,
+  } = useForm<createAdminRegisterType>({
+    resolver: zodResolver(loginAdminSchema),
+  });
+
+  const mutation = useMutation(mut.loginStudent, {
+    onSuccess: (data: ILoginStudentInfo) => {
+      Cookies.set("adminData", JSON.stringify(data));
+      Cookies.set("studentAdminData", JSON.stringify(data.data.user));
+      Cookies.set("adminAccessToken", JSON.stringify(data.data.token));
+      notify();
+      window.location.replace("/admin/profile");
+    },
+
+    onError: () => {
+      router.push("/failed");
+      errorRegister();
+      return;
+},
+  });
+
+  const onHandleSubmit: SubmitHandler<createAdminRegisterType> = (
+    data: ILogin
+  ) => {
+    mutation.mutate(data);
+  };
+
+  return (
+    <>
+      <Header name="Prihlásenie Admin" />
+    </>
+  );
+};
 
 export default LoginForm;
--- src/components/admin/ProfilePage.tsx
+++ src/components/admin/ProfilePage.tsx
@@ -1,13 +1,14 @@
+import { Fragment } from "react";
 import ProfileBody from "./profile/ProfileBody";
 import ProfileHeader from "./profile/ProfileHeader";
 
 const ProfilePage: React.FC = () => {
-    return (
-        <>
-         <ProfileHeader />
-         <ProfileBody />
-        </>
-    )
-}
+  return (
+    <Fragment>
+      <ProfileHeader />
+      <ProfileBody />
+    </Fragment>
+  );
+};
 
 export default ProfilePage;

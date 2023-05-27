971836376e563d418a08b31c797134bd6157a496
Peterdinis <peterdinis0@pobox.sk>
2023-05-27 19:56:08 +0200

    fix: FIxing design in admin profile

--- src/components/admin/ProfilePage.tsx
+++ src/components/admin/ProfilePage.tsx
@@ -3,10 +3,12 @@ import ProfileHeader from "./profile/ProfileHeader";
 
 const ProfilePage: React.FC = () => {
   return (
-    <>
-      <ProfileHeader />
-      <ProfileBody />
-    </>
+    <div className="container mx-auto my-5 p-5">
+      <div className="md:flex no-wrap md:-mx-2">
+        <ProfileHeader />
+        <ProfileBody />
+      </div>
+    </div>
   );
 };
 

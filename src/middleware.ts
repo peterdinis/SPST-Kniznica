25b7c07df367172fcda6b548cdba12228d65005c
Peterdinis <peterdinis0@pobox.sk>
2023-05-28 12:44:41 +0200

    feat: Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import Cookies from "js-cookie";
+import { CREATE_BOOK, CREATE_CATEGORY } from "./constants/routePrefix";
 
 
 export function middleware(request: NextRequest) {
@@ -9,8 +10,10 @@ export function middleware(request: NextRequest) {
     const url = request.nextUrl.clone();
 
     if(teacherCookie || adminCookie) {
-        if(request.nextUrl.pathname.startsWith("/admin")) {
-            
+        if(request.nextUrl.pathname.startsWith(CREATE_BOOK) || request.nextUrl.pathname.startsWith(CREATE_CATEGORY)) {
+            return NextResponse.rewrite(new URL("/", request.url));
         }
+    } else {
+        return NextResponse.redirect("/forbidden");
     }
 }

a40c788d3b10cc05b810c01799382d992aa0ba46
Peterdinis <peterdinis0@pobox.sk>
2023-06-02 18:42:06 +0200

    FOO

--- src/constants/routePrefix.ts
+++
@@ -1,8 +0,0 @@
-export const CREATE_BOOK: string = "/books/create";
-export const CREATE_CATEGORY: string = "/category/create";
-export const CREATE_AUTHOR: string = "/authors/create";
-export const ADMIN_BOOKS: string = "/admin/books/all";
-export const ADMIN_BOOKINGS: string = "/admin/booking/all";
-export const ADMIN_CATEGORIES: string = "/admin/categories/all"
-export const ADMIN_STUDENTS: string = "/admin/students/all";
-export const ADMIN_TEACHERS: string = "/admin/teachers/all";

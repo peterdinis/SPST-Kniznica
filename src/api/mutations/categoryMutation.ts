6eaf51048e7e15f6bd6235aac36fd9a7eb7dd2b5
Peterdinis <peterdinis0@pobox.sk>
2023-06-03 20:38:29 +0200

    feat: Small mutations update

--- src/api/mutations/categoryMutation.ts
+++ src/api/mutations/categoryMutation.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { ICategory } from "../../interfaces/ICategory";
 
-const baseEnv = process.env.NODE_ENV !== "production" ? process.env.NEXT_PUBLIC_BACKEND_URL as string : process.env.NEXT_PUBLIC_PRODUCTION_URL as string
-
 const api = axios.create({
-  baseURL: baseEnv
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
 });
 
 export const createNewCategory = (data: ICategory) => {
--- src/api/mutations/studentMutations.ts
+++ src/api/mutations/studentMutations.ts
@@ -1,13 +1,8 @@
 import { ILogin, IRegister } from "@/interfaces/IStudent";
 import axios from "axios";
 
-const baseEnv =
-  process.env.NODE_ENV !== "production"
-    ? (process.env.NEXT_PUBLIC_BACKEND_URL as string)
-    : (process.env.NEXT_PUBLIC_PRODUCTION_URL as string);
-
 const api = axios.create({
-  baseURL: baseEnv,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
 });
 
 export const register = (data: IRegister) => {

0e51decaad474407a2d960992bc383879e5158be
Peterdinis <peterdinis0@pobox.sk>
2023-06-04 13:48:22 +0200

    FOO

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,28 +1,22 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import useAdmin from "@/hooks/useAdmin";
-import useTeacher from '@/hooks/useTeacher';
 
 const PrivateRoute = <T extends object>(WrappedComponent: React.ComponentType<T>) => {
   const AuthenticatedRoute: React.FC<T> = (props) => {
     const router = useRouter();
-    const teacher = useTeacher();
-    const admin = useAdmin();
 
-    const checkIfIsTeacherOrAdmin = !teacher || !admin;
-
-    console.log(teacher);
-    console.log(admin);
+    // Simulate authentication check
+    const isAuthenticated = true; // Replace with your authentication logic
 
     useEffect(() => {
       // Redirect to login if the user is not authenticated
-      if (!checkIfIsTeacherOrAdmin) {
-        router.push('/forbidden');
+      if (!isAuthenticated) {
+        router.push('/login');
       }
     }, []);
 
     // Render the component only if the user is authenticated
-    return checkIfIsTeacherOrAdmin ? <WrappedComponent {...props} /> : null;
+    return isAuthenticated ? <WrappedComponent {...props} /> : null;
   };
 
   return AuthenticatedRoute;

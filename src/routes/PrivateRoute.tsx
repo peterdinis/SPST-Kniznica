b460ae3a043beca754617fbebd535c7bc03f2376
Peterdinis <peterdinis0@pobox.sk>
2023-06-04 12:52:34 +0200

    FOO

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,9 +1,13 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import useAdmin from "@/hooks/useAdmin";
+import useTeacher from '@/hooks/useTeacher';
 
 const PrivateRoute = <T extends object>(WrappedComponent: React.ComponentType<T>) => {
   const AuthenticatedRoute: React.FC<T> = (props) => {
     const router = useRouter();
+    const teacher = useTeacher();
+    const admin = useAdmin();
 
     // Simulate authentication check
     const isAuthenticated = true; // Replace with your authentication logic

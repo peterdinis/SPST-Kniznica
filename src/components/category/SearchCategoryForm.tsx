ee0d118008fedf067e22cc0878b8ad899d7fc3ea
Peterdinis <peterdinis0@pobox.sk>
2023-06-11 18:36:23 +0200

    Feat: Update category search form style results

--- src/components/category/SearchCategoryForm.tsx
+++ src/components/category/SearchCategoryForm.tsx
@@ -44,7 +44,7 @@ const SearchCategoryForm: React.FC = () => {
   }, []);
   return (
     <>
-          <Header name="Hľadanie konkretnej kategórie" />
+      <Header name="Hľadanie konkretnej kategórie" />
       <div className="flex justify-center align-top">
         <form className="mt-4">
           <input
@@ -82,19 +82,16 @@ const SearchCategoryForm: React.FC = () => {
                 results.data.map((item: ICategory) => {
                   return (
                     <>
-                      <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
-                        <div className="text-center">
-                          <h3 className="text-2xl text-gray-800">
-                            {item.name}
-                          </h3>
-                          <div className="text-center mt-4">
+                      <div className="bg-whiterounded py-5 pl-6 flex items-start shadow">
+                        <div className="pl-3 pr-10 mt-1">
+                          <h3 className="font-normal leading-4 text-red-800 text-2xl break-all">
                             <Link
-                              className="link mt-10 bg-blue-200 p-2 rounded"
-                              href={`/category/detail/${item.id}`}
+                              id="categoryName"
+                              href={`/category/detail/${item.externalId}`}
                             >
-                              Detail
+                              {item.name}
                             </Link>
-                          </div>
+                          </h3>
                         </div>
                       </div>
                     </>
@@ -106,7 +103,7 @@ const SearchCategoryForm: React.FC = () => {
         )}
       </>
     </>
-  )
+  );
 };
 
 export default SearchCategoryForm;

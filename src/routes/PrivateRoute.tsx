1597ce39f82288c82ae33b9f3d95adbfd00123cc
Peterdinis <peterdinis0@pobox.sk>
2023-06-16 20:30:02 +0200

    Fix: Fixing small bug in PrivateRoute component

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,20 +1,24 @@
+import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const PrivateRoute = <T extends object>(WrappedComponent: React.ComponentType<T>) => {
-  const router = useRouter();
+  return (props: T) => {
+    const router = useRouter();
 
-  useEffect(() => {
-    // Check if the cookie exists in session storage
-    const cookie = sessionStorage.getItem('cookieName'); // Replace 'cookieName' with the actual name of your cookie
+    useEffect(() => {
+      // Check if the cookie exists in session storage
+      const teacherCookie = Cookies.get("teacherPersonalInfo");
+      const adminCookie = Cookies.get("adminPersonalData");
 
-    if (!cookie) {
-      // Redirect the user to /forbidden
-      router.push('/forbidden');
-    }
-  }, []);
+      if (!teacherCookie || !adminCookie) {
+        // Redirect the user to /forbidden
+        router.push('/forbidden');
+      }
+    }, []);
 
-  return WrappedComponent;
+    return <WrappedComponent {...props} />;
+  };
 };
 
 export default PrivateRoute;

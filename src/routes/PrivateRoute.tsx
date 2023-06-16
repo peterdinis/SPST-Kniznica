bac75841c889c09e8398d039d22fb454445bb400
Peterdinis <peterdinis0@pobox.sk>
2023-06-16 20:02:44 +0200

    chore: Update package.json

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,24 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const PrivateRoute = <T extends object>(WrappedComponent: React.ComponentType<T>) => {
- /*  const AuthenticatedRoute: React.FC<T> = (props) => {
-    const router = useRouter();
 
-    // Simulate authentication check
-    const isAuthenticated = true; // Replace with your authentication logic
-
-    useEffect(() => {
-      // Redirect to login if the user is not authenticated
-      if (!isAuthenticated) {
-        router.push('/login');
-      }
-    }, []);
-
-    // Render the component only if the user is authenticated
-    return isAuthenticated ? <WrappedComponent {...props} /> : null;
-  };
-
-  return AuthenticatedRoute; */
 };
 
 export default PrivateRoute;

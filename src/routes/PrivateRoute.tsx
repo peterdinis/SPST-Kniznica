e9e80fb325549b15fc4f1b8255302f92699d0409
Peterdinis <peterdinis0@pobox.sk>
2023-06-16 20:21:12 +0200

    fix: WIP Fixing private route

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,7 +2,19 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const PrivateRoute = <T extends object>(WrappedComponent: React.ComponentType<T>) => {
+  const router = useRouter();
 
+  useEffect(() => {
+    // Check if the cookie exists in session storage
+    const cookie = sessionStorage.getItem('cookieName'); // Replace 'cookieName' with the actual name of your cookie
+
+    if (!cookie) {
+      // Redirect the user to /forbidden
+      router.push('/forbidden');
+    }
+  }, []);
+
+  return WrappedComponent;
 };
 
 export default PrivateRoute;

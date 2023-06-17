0d3df5b21c925ad60acadfa0ac8295cd91f52252
Peterdinis <peterdinis0@pobox.sk>
2023-06-17 14:27:16 +0200

    FOO

--- src/components/shared/toasts/bookToasts.ts
+++ src/components/shared/toasts/bookToasts.ts
@@ -5,3 +5,6 @@ export const errorRegister = () =>
   toast.error(
     "Objednávka nebola vytvorená. Skontroluje prosím údajte či sú správne"
   );
+
+export const createNotify = () => toast.success("Kniha bola vytvorená");
+export const createErrorRegister = () => toast.error("Kniha nebola vytvorená");

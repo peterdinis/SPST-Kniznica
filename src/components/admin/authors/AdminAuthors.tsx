6083966468d9ca26cbb371170c0b6fcd010465e7
Peterdinis <peterdinis0@pobox.sk>
2023-06-17 18:26:43 +0200

    feat: Start to updating admin tables wip

--- src/components/admin/authors/AdminAuthors.tsx
+++ src/components/admin/authors/AdminAuthors.tsx
@@ -1,4 +1,25 @@
+import { useTable, Column, useGlobalFilter, usePagination } from 'react-table';
+import { useQuery } from '@tanstack/react-query';
+import Header from '@/components/shared/Header';
+import * as api from "@/api/queries/authorQueries"
+import {IUpdateAuthor } from '@/interfaces/IAuthor';
+import FallbackLoader from '@/components/shared/FallbackLoader';
+import FallbackRender from '@/components/shared/errors/FallbackRender';
+import { getAllAuthorsError } from '@/components/shared/errors/constants/errorMessages';
+
 const AdminAuthors: React.FC = () =>{
+    const { data, isLoading, isError } = useQuery<IUpdateAuthor>(["getAllAdminAuthors"], api.getAuthors);
+
+    if (isLoading) {
+        return <FallbackLoader />
+      }
+    
+    if (isError) {
+        return <FallbackRender error={getAllAuthorsError} />
+    }
+
+    const columns: Column<IUpdateAuthor>[] = [];
+
     return (
         <>
         rrrr

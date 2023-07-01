53eb4f15136629ed16120861548ada0aeb6cf535
Peterdinis <peterdinis0@pobox.sk>
2023-07-01 11:47:49 +0200

    feat: Update about wrapper text

--- src/components/about/AboutWrapper.tsx
+++ src/components/about/AboutWrapper.tsx
@@ -8,10 +8,7 @@ const AboutWrapper: React.FC = () => {
       <Paper elevation={3}>
         <h1 className="text-2xl text-red-700 text-center mt-12">Správca je Mgr. Nataša Dzubaková</h1>
         <p className="whitespace-pre-line text-2xl mt-8 ml-2 p-0">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sequi et
-          nesciunt sed excepturi quaerat, incidunt a architecto molestiae! Saepe
-          iusto temporibus eaque voluptate officiis facere veniam amet
-          praesentium id.
+          Knižnicu nájdete smerom na internát na 3 poschodí.
         </p>
       </Paper>
     </>

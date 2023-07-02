769776970ad7a4e3c5125f0710a60dcbd2a55064
Peterdinis <peterdinis0@pobox.sk>
2023-07-02 15:40:28 +0200

    style: Updating ProfileBody

--- src/components/teacher/profile/ProfileBody.tsx
+++ src/components/teacher/profile/ProfileBody.tsx
@@ -35,7 +35,7 @@ const ProfileBody: React.FC = () => {
 
   return (
     <>
-      <div className="w-full md:w-9/12 mx-2 h-64">
+      <div className="w-full md:w-9/12 mx-2 h-128">
         <div className="bg-white p-3 shadow-sm rounded-sm">
           <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
             <PersonIcon />

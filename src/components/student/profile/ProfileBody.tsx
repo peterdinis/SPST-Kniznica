55e1291306e950a632fa564f553ac2ecd2d6392b
Peterdinis <peterdinis0@pobox.sk>
2023-07-04 20:14:38 +0200

    fix: Fixing type in Update profil email

--- src/components/student/profile/ProfileBody.tsx
+++ src/components/student/profile/ProfileBody.tsx
@@ -139,7 +139,7 @@ const ProfileBody: React.FC = () => {
                         Email
                       </label>
                       <input
-                        type="temail"
+                        type="email"
                         className="outline-none mt-2 block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500"
                         placeholder="Používateľské meno"
                         value={student?.data.user.email}

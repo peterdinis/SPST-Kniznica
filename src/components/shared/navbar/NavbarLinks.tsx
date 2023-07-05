8a6b2938df5ea94b1417394f2e1af4a77f2ab8d0
Peterdinis <peterdinis0@pobox.sk>
2023-07-05 11:51:17 +0200

    feat: Create new notification dropdown component

--- src/components/shared/navbar/NavbarLinks.tsx
+++ src/components/shared/navbar/NavbarLinks.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import useStudent from "@/hooks/useStudent";
 import useTeacher from "@/hooks/useTeacher";
 import useAdmin from "@/hooks/useAdmin";
+import NotificationDropdown from "../notifications/NotificationDropdown";
 
 const NavbarLinks: React.FC = () => {
   const { student } = useStudent();
@@ -43,6 +44,9 @@ const NavbarLinks: React.FC = () => {
           <li className="text-black text-xl">
             <Link href="/student/profile">Profil</Link>
           </li>
+          <li className="text-black text-xl">
+            <NotificationDropdown />
+          </li>
         </>
       )}
 
@@ -51,6 +55,9 @@ const NavbarLinks: React.FC = () => {
           <li className="text-black text-xl">
             <Link href="/teacher/profile">Profil</Link>
           </li>
+          <li className="text-black text-xl">
+            <NotificationDropdown />
+          </li>
         </>
       )}
 
@@ -58,6 +65,9 @@ const NavbarLinks: React.FC = () => {
         <>
           <li className="text-black text-xl">
             <Link href="/admin/profile">Profil</Link>
+          </li>
+          <li className="text-black text-xl">
+            <NotificationDropdown />
           </li>
         </>
       )}
---
+++ src/components/shared/notifications/NotificationDropdown.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Notifications } from '@mui/icons-material';
+
+const NotificationDropdown: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const messages = [
+    'Message 1',
+    'Message 2',
+    'Message 3',
+    // Add more messages as needed
+  ];
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        className="flex items-center justify-center text-gray-600 hover:text-gray-900 focus:outline-none"
+        onClick={toggleDropdown}
+      >
+        <Notifications className="w-6 h-6" /> 0
+      </button>
+
+      {isOpen && (
+        <div className="absolute right-0 mt-2 bg-white border rounded shadow">
+          <ul className="py-2">
+            {messages.map((message, index) => (
+              <li key={index} className="px-4 py-2 hover:bg-gray-100">
+                {message}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NotificationDropdown;

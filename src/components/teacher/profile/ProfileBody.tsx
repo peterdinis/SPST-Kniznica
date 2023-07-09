ea3c15d5fdb6f866bd6346ee69eeea3a4706bcd4
Peterdinis <peterdinis0@pobox.sk>
2023-07-09 12:46:18 +0200

    style: Updating profile-body

--- src/components/teacher/profile/ProfileBody.tsx
+++ src/components/teacher/profile/ProfileBody.tsx
@@ -59,7 +59,7 @@ const ProfileBody: React.FC = () => {
                 <div className="px-4 py-2 font-semibold">
                   Moje požičané knihy
                 </div>
-                <div className="px-4 py-2 text-red-700">
+                <div className="px-4 py-2 text-red-500">
                   <button onClick={goToMyBooks}>Moje knihy</button>
                 </div>
               </div>

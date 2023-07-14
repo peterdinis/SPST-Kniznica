97c8b01a762f7772195585ea4018537901248002
Peterdinis <peterdinis0@pobox.sk>
2023-07-14 21:49:48 +0200

    feat: FOO

--- src/components/shared/toasts/registerToasts.ts
+++ src/components/shared/toasts/registerToasts.ts
@@ -2,6 +2,3 @@ import { toast } from "react-toastify";
 
 export const notify = () => toast.success("Registrácia bola úspešná");
 export const errorRegister = () => toast.error("Registrácia nebola úspešná");
-export const userAlreadyExistsToast = () => toast.error("Používateľ už existuje");
-export const emailAlreadyExistsToast = () => toast.error("Email už existuje");
-export const applicationErrorToast = () => toast.error("Nepodarilo sa vykonať request");

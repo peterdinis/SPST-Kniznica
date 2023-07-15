af9a8f8765d3ec7afdfbdc34e119f64d1e7506be
Peterdinis <peterdinis0@pobox.sk>
2023-07-15 13:06:34 +0200

    feat: Notifications wip

--- src/components/shared/notifications/NotificationDropdown.tsx
+++ src/components/shared/notifications/NotificationDropdown.tsx
@@ -1,12 +1,13 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Notifications } from "@mui/icons-material";
 import { logoutToast } from "../toasts/adminToasts";
 import Cookies from "js-cookie";
 import { useQuery } from "@tanstack/react-query";
 import * as api from "@/api/queries/notificationsQueries";
-import useStudent from "@/hooks/useStudent"
+import useStudent from "@/hooks/useStudent";
 import FallbackLoader from "../FallbackLoader";
 import FallbackRender from "../errors/FallbackRender";
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
 
 const NotificationDropdown: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -33,11 +34,15 @@ const NotificationDropdown: React.FC = () => {
     window.location.replace("/student/login");
   };
 
-/*   const studentUsername = studentPersonalInfo?.username;
+  const studentUsername = studentPersonalInfo?.username;
 
-  const {data, isLoading, isError} = useQuery(["myNotifications", studentUsername], () => {
-    api.getMyNotifications(studentUsername as unknown as string)
-  })
+  const { data, isLoading, isError } = useQuery(
+    ["myNotifications", studentUsername],
+    async () => {
+      const response = await api.getMyNotifications(studentUsername as string);
+      return response?.data ?? [];
+    }
+  );
 
   if (isError) {
     return <FallbackRender error={"Nepodarilo sa načítať notifikácie"} />;
@@ -47,8 +52,8 @@ const NotificationDropdown: React.FC = () => {
     return <FallbackLoader />;
   }
 
-  console.log(data); */
-  
+  console.log(data);
+
   return (
     <div className="relative z-20">
       <button
@@ -62,7 +67,16 @@ const NotificationDropdown: React.FC = () => {
         <div className="absolute right-0 mt-2 bg-white border rounded shadow z-30">
           <ul className="py-2">
             <li className="px-4 py-2 hover:bg-gray-100">
-             {/*  {notifications.map((notification: any) => (
+              {data.length === 0 && (
+                <div>
+                  {" "}
+                  <dd className="mt-3 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    Žiadne správy{" "}
+                    <SentimentDissatisfiedIcon />
+                  </dd>
+                </div>
+              )}
+              {/*  {notifications.map((notification: any) => (
                 <li key={notification.id}>
                   <h3>{notification.title}</h3>
                   <p>{notification.message}</p>

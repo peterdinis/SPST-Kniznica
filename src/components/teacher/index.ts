0036768bc6b53cafcd418af736df362d82b0add7
Peterdinis <peterdinis0@pobox.sk>
2023-07-15 20:30:52 +0200

    feat: Adding next-dynamic wip

--- src/components/teacher/index.ts
+++ src/components/teacher/index.ts
@@ -3,3 +3,8 @@ import dynamic from "next/dynamic";
 export const LoginForm = dynamic(() => import("./LoginForm"));
 export const MyProfile = dynamic(() => import("./MyProfile"));
 export const RegisterForm = dynamic(() => import("./RegisterForm"));
+export const MyBooks = dynamic(() => import("./profile/MyBooks"));
+export const Options = dynamic(() => import("./profile/Options"));
+export const ProfileBody = dynamic(() => import("./profile/ProfileBody"));
+export const ProfileHeader = dynamic(() => import("./profile/ProfileHeader"));
+export const ReturnBookModal = dynamic(() => import("./profile/ReturnBookModal"));

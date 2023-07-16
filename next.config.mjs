d1b94e615dd4892697cb438172d8d7452958991e
Peterdinis <peterdinis0@pobox.sk>
2023-07-16 10:53:46 +0200

    FOO

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-import million from "million/compiler";
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default million.next(nextConfig);
+export default nextConfig;

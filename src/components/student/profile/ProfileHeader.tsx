d9ed02c2407619af54d0656940871ffa4d6ec592
Peterdinis <peterdinis0@pobox.sk>
2023-07-22 17:33:33 +0200

    feat: Updating ProfileHeader

--- src/components/student/profile/ProfileHeader.tsx
+++ src/components/student/profile/ProfileHeader.tsx
@@ -18,10 +18,9 @@ const ProfileHeader: React.FC = () => {
     setSelectedFile(file);
   };
 
-  
   const handleClose = () => {
     setSelectedFile(null);
-    setIsModalOpen(false); 
+    setIsModalOpen(false);
   };
 
   const handleUpload = async () => {
@@ -79,37 +78,35 @@ const ProfileHeader: React.FC = () => {
         >
           Nová fotka
         </button>
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-4">Nová fotka</h2>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleFileChange}
-              ref={fileInputRef}
-            />
-            <div className="flex justify-between mt-4">
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        
+        {isModalOpen && (
+          <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl font-bold mb-4">Nová fotka</h2>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+                ref={fileInputRef}
                 onClick={() => fileInputRef.current?.click()}
-              >
-                Vybrať obrázok
-              </button>
+              />
+              <div className="flex mt-4">
+                <button
+                  className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                  onClick={handleClose}
+                >
+                  Zavrieť
+                </button>
+              </div>
               <button
-                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                onClick={handleClose}
+                className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+                onClick={handleUpload}
               >
-                Zavrieť
+                Nahrať
               </button>
             </div>
-            <button
-              className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-              onClick={handleUpload}
-            >
-              Nahrať
-            </button>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );

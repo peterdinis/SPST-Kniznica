0b457395ba59a213acd578f06a36815a9544a4be
Peterdinis <peterdinis0@pobox.sk>
2023-07-28 17:21:40 +0200

    fix: Adding missing field to create author

--- src/components/authors/CreateAuthorForm.tsx
+++ src/components/authors/CreateAuthorForm.tsx
@@ -78,6 +78,16 @@ const CreateAuthorForm: React.FC = () => {
           </div>
           <div className="relative z-0 mb-6 group">
             <Input
+              type="text"
+              className="mt-4 block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              {...register("country", {
+                required: true,
+              })}
+              placeholder="Krajina pôvodu autora"
+            />
+          </div>
+          <div className="relative z-0 mb-6 group">
+            <Input
               type="number"
               className="mt-4 block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               {...register("birthYear", {

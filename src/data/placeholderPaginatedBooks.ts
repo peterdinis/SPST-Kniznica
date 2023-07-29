418e584680662af80825c7e44abd1342ead2d538
Peterdinis <peterdinis0@pobox.sk>
2023-07-29 15:25:57 +0200

    style: Foramtting data folder wip

--- src/data/placeholderPaginatedBooks.ts
+++ src/data/placeholderPaginatedBooks.ts
@@ -1,38 +1,38 @@
 export interface IPaginatedBooks {
-    data: {
-        result: {
-            0: {
-                name: "Random Name",
-                description: "Random Author",
-                author: "Random Author",
-                year: 2020,
-                available: false,
-                pages: 222,
-                publisher: "Random Publisher",
-                image: "Random Image"
-            },
+  data: {
+    result: {
+      0: {
+        name: "Random Name";
+        description: "Random Author";
+        author: "Random Author";
+        year: 2020;
+        available: false;
+        pages: 222;
+        publisher: "Random Publisher";
+        image: "Random Image";
+      };
 
-            1: {
-                name: "Random Name",
-                description: "Random Author",
-                author: "Random Author",
-                year: 2020,
-                available: false,
-                pages: 222,
-                publisher: "Random Publisher",
-                image: "Random Image"
-            },
+      1: {
+        name: "Random Name";
+        description: "Random Author";
+        author: "Random Author";
+        year: 2020;
+        available: false;
+        pages: 222;
+        publisher: "Random Publisher";
+        image: "Random Image";
+      };
 
-            2: {
-                name: "Random Name",
-                description: "Random Author",
-                author: "Random Author",
-                year: 2020,
-                available: false,
-                pages: 222,
-                publisher: "Random Publisher",
-                image: "Random Image"
-            }
-        }
-    }
+      2: {
+        name: "Random Name";
+        description: "Random Author";
+        author: "Random Author";
+        year: 2020;
+        available: false;
+        pages: 222;
+        publisher: "Random Publisher";
+        image: "Random Image";
+      };
+    };
+  };
 }

87847fa47252faa168dea8f6cbc54ad7898bf2c6
Peterdinis <peterdinis0@pobox.sk>
2023-07-29 15:28:49 +0200

    feat: Cleaning api folder

--- src/api/mutations/teacherMutations.ts
+++ src/api/mutations/teacherMutations.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ILogin, INewPasswordTeacher, IRegister, IUpdateTeacher } from "@/interfaces/ITeacher";
+import { ILogin, IRegister} from "@/interfaces/ITeacher";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL as string,
@@ -13,26 +13,6 @@ export const login = (data: ILogin) => {
 return api.post("teacher/login", data);
 }
 
-export const teacherChangePassword = (teacherData: INewPasswordTeacher) => {
-  return api.patch(`teacher/password/${teacherData.username}/new`,teacherData.newPassword);
-};
-
-export const updateProfile = (data: IUpdateTeacher, username: string) => {
-  return api.patch(`teacher/profile/update/${username}`, data)
-}
-
 export const deleteProfile = (username: string) => {
   return api.delete(`teacher/profile/update/${username}`);
-}
-
-export const studentChangePassword = (teacherData: INewPasswordTeacher) => {
-  return api.patch(`student/password/${teacherData.username}/new`, teacherData.newPassword);
-};
-
-export const deleteMessage = (id: number) => {
-  if(!id) {
-    return;
-  }
-
-  return api.delete(`message/delete/${id}`);
 }

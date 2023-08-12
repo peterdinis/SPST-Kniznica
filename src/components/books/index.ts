f304b3a16b390d1d0ad2022748ac90fc94c70c84
Peterdinis <peterdinis0@pobox.sk>
2023-08-12 09:21:36 +0200

    feat: Disable ssr in some components wip

--- src/components/books/index.ts
+++ src/components/books/index.ts
@@ -3,5 +3,5 @@ import dynamic from "next/dynamic";
 export const AllBooks = dynamic(() => import("./AllBooks"));
 export const BookInfo = dynamic(() => import("./BookInfo"));
 export const CreateBookForm = dynamic(() => import("./CreateBookForm"));
-export const Reasons = dynamic(() => import("./Reasons"));
+export const Reasons = dynamic(() => import("./Reasons"), {ssr: false});
 export const SearchBookForm = dynamic(() => import("./SearchBookForm"));
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,13 +1,13 @@
 import dynamic from "next/dynamic";
 
-export const Seo = dynamic(() => import("./Seo"));
-export const Mailto = dynamic(() => import("./Mailto"));
+export const Seo = dynamic(() => import("./Seo"), {ssr: false});
+export const Mailto = dynamic(() => import("./Mailto"), {ssr: false});
 export const Layout = dynamic(() => import("./Layout"));
-export const Header = dynamic(() => import("./Header"));
-export const Footer = dynamic(() => import("./Footer"));
+export const Header = dynamic(() => import("./Header"),{ssr: false});
+export const Footer = dynamic(() => import("./Footer"),{ssr: false});
 export const FallbackLoader = dynamic(() => import("./FallbackLoader"));
-export const DateTimePick = dynamic(() => import("./DateTimePicker"));
-export const Creator = dynamic(() => import("./Creator"));
+export const DateTimePick = dynamic(() => import("./DateTimePicker"), {ssr: false});
+export const Creator = dynamic(() => import("./Creator"), {ssr: false});
 export const Navbar = dynamic(() => import("./navbar/Navbar"));
 export const NavbarLinks = dynamic(() => import("./navbar/NavbarLinks"));
 export const FallbackRender = dynamic(() => import("./errors/FallbackRender"));

7b3865909ec6525ad33b9ff2f9d4b490fd0788fd
Peterdinis <peterdinis0@pobox.sk>
2023-08-26 21:01:08 +0200

    feat: Moving links to env

--- src/env.mjs
+++ src/env.mjs
@@ -4,11 +4,13 @@ import { z } from "zod";
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_BACKEND_URL: z.string().min(1),
-    NEXT_PUBLIC_EDUPAGE_URL: z.string().min(1)
+    NEXT_PUBLIC_EDUPAGE_URL: z.string().min(1),
+    NEXT_PUBLIC_SCHOLL_URL: z.string().min(1)
   },
   runtimeEnv: {
     NEXT_PUBLIC_BACKEND_URL:
       process.env.NEXT_PUBLIC_BACKEND_URL,
-      NEXT_PUBLIC_EDUPAGE_URL: process.env.NEXT_PUBLIC_EDUPAGE_URL
+      NEXT_PUBLIC_EDUPAGE_URL: process.env.NEXT_PUBLIC_EDUPAGE_URL,
+      NEXT_PUBLIC_SCHOLL_URL: process.env.NEXT_PUBLIC_SCHOLL_URL
   },
 });

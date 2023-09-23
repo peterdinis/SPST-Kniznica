1fd39528ad3bc0314aa88567b0d5adc22a3fb6b3
Peterdinis <peterdinis0@pobox.sk>
2023-09-23 13:42:56 +0200

    FOO

--- src/interfaces/IModal.ts
+++ src/interfaces/IModal.ts
@@ -3,7 +3,7 @@ import { ReactElement, ReactNode } from "react";
 export interface FadeProps {
   children: ReactElement;
   in?: boolean;
-  onClick?: () =>void;
+  onClick?: () => void;
   onEnter?: (node: HTMLElement, isAppearing: boolean) => void;
   onExited?: (node: HTMLElement, isAppearing: boolean) => void;
   ownerState?: string;
